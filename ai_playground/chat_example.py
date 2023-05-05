4d09c9ccc577cec55460d2366b67fa586053ddb3
shibayu36 <shibayu36@gmail.com>
2023-05-05 16:52:51 +0900

    Arrange

--- ai_playground/chat_example.py
+++ ai_playground/chat_example.py
@@ -24,27 +24,26 @@ while True:
     user_message = input("You: ")
     messages.append({"role": "user", "content": user_message})
 
+    print("Assistant: ", end="", flush=True)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.5,
         stream=True,
     )
-
-    print("Assistant: ", end="", flush=True)
-
     current_message = ""
     for chunk in response:
         delta_content = chunk["choices"][0]["delta"].get("content", "")
         print(delta_content, end="", flush=True)
         current_message += delta_content
+    print()
 
     messages.append({"role": "assistant", "content": current_message})
-    print()
 
     total_token_count = get_total_token_count(messages)
     print(f"Total token count: {total_token_count}")
 
+    # Delete old messages if messages are about to exceed the limit
     while total_token_count > 2500:
         print("Delete old context")
         del messages[2]
