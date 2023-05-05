77ff29a5977b9bd022b82a6c627bad2e8b64a21b
shibayu36 <shibayu36@gmail.com>
2023-05-05 14:17:22 +0900

    Update parse-movable-type.py

--- parse-movable-type.py
+++ parse-movable-type.py
@@ -1,6 +1,12 @@
+import pdb
+from http.client import HTTPConnection  # py3
+import logging
 from pprint import pprint
 import re
 from collections import defaultdict
+from html2text import html2text
+from llama_index import Document, GPTSimpleVectorIndex, download_loader
+from dotenv import load_dotenv
 
 
 def parse_movable_type_entry(entry_text):
@@ -31,7 +37,7 @@ def parse_movable_type_entry(entry_text):
 
 def extract_entries_from_movable_type(movable_type_text):
     entry_texts = re.split(r"(?m)^--------\n", movable_type_text)
-    entry_texts.pop()
+    entry_texts.pop()  # Delete last empty entry
 
     entries = [parse_movable_type_entry(entry_text)
                for entry_text in entry_texts]
@@ -39,7 +45,38 @@ def extract_entries_from_movable_type(movable_type_text):
     return entries
 
 
+load_dotenv()
+
 with open('/Users/yuki.shibazaki/Downloads/blog.shibayu36.org.export.txt', "r", encoding="utf-8") as file:
     content = file.read()
 
 entries = extract_entries_from_movable_type(content)
+
+
+log = logging.getLogger('urllib3')
+log.setLevel(logging.DEBUG)
+
+# logging from urllib3 to console
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+log.addHandler(ch)
+
+# print statements from `http.client.HTTPConnection` to console/stdout
+HTTPConnection.debuglevel = 1
+
+# index = GPTSimpleVectorIndex.load_from_disk('index.json')
+# for entry in entries:
+#     print(entry['title'])
+#     doc = Document(
+#         html2text(entry['body']),
+#         extra_info={'title': entry['title']}
+#     )
+#     index.insert(doc)
+#     index.save_to_disk('index.json')
+
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+pdb.set_trace()
+response = index.query("YAPCとは何？")
+print(response)
+for node in response.source_nodes:
+    print(node.node.extra_info['title'])
